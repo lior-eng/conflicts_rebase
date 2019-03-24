cdcabbd5486f608a8bf8211798a6dbba6ec6bf58
Omri Spector <omri.spector@gmail.com>
2019-03-24 17:26:17 +0200

    division rules

--- calculator.py
+++ calculator.py
@@ -25,7 +25,6 @@ print("4.Divide")
 
 # Take input from the user 
 choice = input("Enter choice(1/2/3/4):")
-
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 
@@ -33,7 +32,11 @@ if choice == '1':
    print(num1,"+",num2,"=", add(num1,num2))
 elif choice == '2':
    print(num1,"-",num2,"=", subtract(num1,num2))
+elif choice == '3':
+   print(num1,"*",num2,"=", multiply(num1,num2))
+
+elif choice == '4':
+   print(num1,"/",num2,"=", divide(num1,num2))
 
 else:
    print("Invalid input ",choice)
-
